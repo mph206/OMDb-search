d825bf48daddeb8526974ae646e5f591c8c93b8b
Martin Harrison <mpharrison206@gmail.com>
2020-07-29 22:20:20 +0100

    Updates

---
+++ precache-manifest.e286be881a89c90a588ba5caafc91c0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b5f040f4f50eccf10332ebf04938ef",
+    "url": "/OMDb-search/index.html"
+  },
+  {
+    "revision": "a9e5c4f84dc99f41a2b5",
+    "url": "/OMDb-search/static/css/main.5b23a74d.chunk.css"
+  },
+  {
+    "revision": "2ec1143a93391a5eb7d7",
+    "url": "/OMDb-search/static/js/2.206e6029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OMDb-search/static/js/2.206e6029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e5c4f84dc99f41a2b5",
+    "url": "/OMDb-search/static/js/main.ff943ed5.chunk.js"
+  },
+  {
+    "revision": "3737064538766c363aeb",
+    "url": "/OMDb-search/static/js/runtime-main.64a60cf1.js"
+  },
+  {
+    "revision": "fafe155046397ce60e108549cfbdff00",
+    "url": "/OMDb-search/static/media/IMDb.fafe1550.svg"
+  },
+  {
+    "revision": "bc8586653ad856be71189d415b170a93",
+    "url": "/OMDb-search/static/media/Metacritic.bc858665.svg"
+  },
+  {
+    "revision": "d7ea325d74ca23f65e13d24c5b1b714b",
+    "url": "/OMDb-search/static/media/Rotten_Tomatoes.d7ea325d.svg"
+  },
+  {
+    "revision": "4e445a76bec31eca3ba77f3f03a863af",
+    "url": "/OMDb-search/static/media/search-solid.4e445a76.svg"
+  },
+  {
+    "revision": "58c8bfe1d7beb0c5b4ec2268da06e554",
+    "url": "/OMDb-search/static/media/sort-numeric-down.58c8bfe1.svg"
+  },
+  {
+    "revision": "81eeb09d9bca171205b2a5acff96664c",
+    "url": "/OMDb-search/static/media/sort-numeric-up.81eeb09d.svg"
+  }
+]);
