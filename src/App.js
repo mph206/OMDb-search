0a1fb0f3dbc93a31fb53e3ff5b5119ddea862a40
Martin Harrison <mpharrison206@gmail.com>
2020-07-29 11:50:42 +0100

    search by title and year

--- src/App.js
+++ src/App.js
@@ -18,9 +18,15 @@ class App extends Component {
   }
   
   searchTitle = (event) => {
-    const searchTerm = event.target.value.split(' ').join('+'); // need to add year
-    console.log(searchTerm);
-    fetch(`http://www.omdbapi.com/?apikey=${this.state.apiKey}&type=movie&s=${searchTerm}`)
+    const indexOfYearStart = event.target.value.search(/[0-9][0-9][0-9][0-9]/);
+    let searchArray = [event.target.value];
+    console.log(indexOfYearStart)
+    indexOfYearStart !== -1 && indexOfYearStart === 0 
+    ? searchArray = [event.target.value.substring(5).split(' ').join('+'), event.target.value.substring(0,4)]
+    : searchArray = [event.target.value.substring(0, indexOfYearStart - 1).split(' ').join('+'), event.target.value.substring(indexOfYearStart)]
+    // console.log(searchArray)
+    // console.log(`http://www.omdbapi.com/?apikey=${this.state.apiKey}&type=movie&s=${searchArray[0]}&y=${searchArray[1]}`);
+    fetch(`http://www.omdbapi.com/?apikey=${this.state.apiKey}&type=movie&s=${searchArray[0]}&y=${searchArray[1]}`)
     .then(response => response.json())
     .then(data => this.setState({movieData: data.Search}))
     .catch(error => console.log(error))
