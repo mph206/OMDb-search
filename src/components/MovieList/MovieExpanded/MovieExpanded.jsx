cdbe4b42c6d5ed0f23a4adfca757da88de31926d
Martin Harrison <mpharrison206@gmail.com>
2020-07-29 15:57:59 +0100

    Add ratings to expanded data

--- src/components/MovieList/MovieExpanded/MovieExpanded.jsx
+++ src/components/MovieList/MovieExpanded/MovieExpanded.jsx
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
 import styles from './MovieExpanded.module.scss';
 import Placeholder from '../MovieCard/Placeholder.png';
+import Rotten from './Rotten_Tomatoes.svg';
+import IMDb from './IMDb.svg';
+import Metacritic from './Metacritic.svg';
+
 
 class MovieExpanded extends Component {
 
     state = {
-        extendedData: {}
+
     }
+
     componentDidMount() {
-        console.log(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${this.props.movieData.imdbID}`)
         fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${this.props.movieData.imdbID}`)
         .then(response => response.json())
         .then(data => {this.setState({extendedData: data})})
         .catch(error => console.log(error))
-    }
-
+    }   
+    
+    //SX300
     renderData = () => {
         if (this.state.extendedData) { 
             const { Poster, Title, Year, Rated, Plot, Genre, Ratings } = this.state.extendedData;
             return (
                 <div className={styles.dataContainer}>
-                    {/* <div className={styles.imageDiv}> */}
-                        <img src={Poster !== 'N/A' ? Poster : Placeholder} alt="Movie Poster"/>
-                    {/* </div> */}
+                    <img src={Poster !== 'N/A' ? Poster : Placeholder} alt="Movie Poster"/> 
                     <div className={styles.movieText}>
                         <h3>{Title} ({Year})</h3>
                         <p>{Plot}</p>
                         <p>{Genre}</p>
+                        <section className={styles.ratings}>
+                            {this.mapRatings()}
+                        </section>
                     </div>
                 </div>
             )
         }
+    }
+
+    mapRatings = () => {
+        return this.state.extendedData.Ratings.map((item, index) => {
+            return (
+                <div className={styles.rating} key={index}>
+                    {this.getRatingLogo(item.Source)}
+                    <p>{item.Value}</p>
+                </div>
+            )
+        })
+    }
+
+    getRatingLogo = (source) => {
+        if (source === 'Rotten Tomatoes') return <img src={Rotten} alt='Rotten Tomatoes'></img> 
+        else if (source === 'Metacritic') return <img src={Metacritic} alt='Metacritic'></img>
+        else if (source === 'Internet Movie Database') return <img src={IMDb} alt='IMDb'></img> 
+        else return <p>{source}</p>
     }
     
     render() { 
